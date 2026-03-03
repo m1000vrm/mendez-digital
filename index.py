09e3cc6f774703abf8197573517e4f221cb00e63
m1000vrm <mv069411@gmail.com>
2026-03-03 19:36:17 -0300

    teste2
    teste2

---
+++ index.py
@@ -0,0 +1,11 @@
+frutas = ['maça', 'banana','pera', 'uva','laranja','manga']
+
+'''print (frutas[0])
+print (frutas[1])
+print (frutas[2])
+print (frutas[3])
+print (frutas[4])
+print (frutas[5])'''
+
+print (frutas[1:4])
+print (frutas[3:5])
